de5894e5de8ea82c3422a481dcb2352a95bea9ea
Yuri Toledo <yuriwtoledo@gmail.com>
2021-05-20 13:30:54 -0300

    chore: add eslint and remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,11 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
