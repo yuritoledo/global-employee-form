b94533db8452a8902556fb746b413c8c03179e75
Yuri Toledo <yuriwtoledo@gmail.com>
2021-05-20 17:14:25 -0300

    refac: added more components

--- src/pages/EmployeeForm/styles.ts
+++ src/pages/EmployeeForm/styles.ts
@@ -1,5 +1,31 @@
 import styled from 'styled-components'
 
-export const Container = styled.main``
-export const Form = styled.form``
-export const Button = styled.button``
+export const Title = styled.h1`
+  font-size: 2rem;
+  padding-bottom: 15px;
+`
+
+export const Container = styled.main`
+  background-color: #FFF;
+  border-radius: 8px;
+  padding: 20px;
+`
+
+export const Form = styled.form`
+  width: 350px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+
+export const Button = styled.button`
+  font-weight: bold;
+  font-size: 1.1rem;
+  width: 100%;
+  border: none;
+  background-color: #080;
+  border-radius: 4px;
+  color: #FFF;
+  padding: 8px 0;
+  margin: 15px 0;
+`
