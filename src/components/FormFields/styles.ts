16eeb1e306d6540874d80bb1f81dedce879207b4
Yuri Toledo <yuriwtoledo@gmail.com>
2021-05-20 17:13:23 -0300

    feat: create styles for FormFields

---
+++ src/components/FormFields/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 10rem;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 7px 0;
+`
+
+export const Input = styled.input`
+  border: 1px solid black;
+  border-radius: 2px;
+  padding: 5px;
+`
+
+export const ExtendedLabel = styled.span`
+  margin-left: 3px;
+  color: grey;
+`
