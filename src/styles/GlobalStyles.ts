4e0593c78141e53f8adaa80501573db970abcd47
Yuri Toledo <yuriwtoledo@gmail.com>
2021-05-20 17:14:14 -0300

    feat: create global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    font-size: 1rem;
+    box-sizing: border-box;
+    font-family: Nunito;
+  }
+`
+
+export default GlobalStyle
