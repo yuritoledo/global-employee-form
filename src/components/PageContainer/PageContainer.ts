af8de476179f81deb514fdb16079819d4551a554
Yuri Toledo <yuriwtoledo@gmail.com>
2021-05-20 17:13:30 -0300

    feat: create PageContainer component

---
+++ src/components/PageContainer/PageContainer.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const PageContainer = styled.div`
+  background-color: #EEE;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default PageContainer
